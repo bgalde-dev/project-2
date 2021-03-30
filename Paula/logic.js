64a68c305084176ed47d4dc6e3c2a3d29db8384f
Paula Lopes <paulalopes@Paulas-MacBook-Air.local>
2021-03-29 21:49:07 -0700

    paula's csv to resources

---
+++ Paula/logic.js
@@ -0,0 +1,111 @@
+//  Read in average_salary.csv
+d3.csv("data/average_salary.csv").then(function (data) {
+    console.log(data);
+});
+
+// Build Chart
+function buildCharts(sample) {
+
+    // Data Samples
+    d3.csv("data/average_salary.csv", function (data) {
+        var sampleInfo = data.average_salary;
+        console.log(sampleInfo)
+
+        // Filter object by ID Industry Group
+        var results = sampleInfo.filter(sampleObj => sampleObj.IDIndustryGroup == sample)[0];
+        console.log(results)
+
+        // // Set variables 
+        // var sampleValues = results.sample_values.slice(0, 10).reverse();
+        // console.log(sampleValues);
+
+        // var otuIDs = results.otu_ids;
+        // console.log(otuIDs);
+
+        // var otuLabels = results.otu_labels.slice(0, 10).reverse();
+        // console.log(otuLabels);
+
+        // var yticks = otuIDs.map(sampleObj => "OTU " + sampleObj).slice(0,10).reverse();
+        
+        // // Bar chart
+        // var barData = [{
+        //     x: sampleValues,
+        //     y: yticks,
+        //     text: otuLabels,
+        //     type: "bar",
+        //     orientation: "h"
+
+        // }];
+
+        // //Plot
+        // Plotly.newPlot("bar", barData);
+
+        // // Bubble Chart
+        // d3.json("samples.json").then((data) => {
+        //     var sampleInfo = data.samples;
+        //     var results = sampleInfo.filter(sampleObj => sampleObj.id == sample)[0];
+            
+        //     var bubbleData =  [{
+        //         x: results.otu_ids,
+        //         y: results.sample_values,
+        //         mode: "markers",
+        //         text: results.otu_labels,
+        //         marker: {
+        //             size: results.sample_values,
+        //             color: results.otu_ids
+        //          }
+        //     }]; 
+        
+        //     //Plot
+        //     Plotly.newPlot("bubble", bubbleData);
+        // });
+    });
+}
+
+
+// // Drop down menu
+// function init() {
+//     var dropDown = d3.selectAll("#selDataset");
+
+//     // Add sample names to a variable
+//     d3.json("samples.json").then((data) => {
+//         var sampleNames = data.names;
+
+//         sampleNames.forEach((sample) => {
+//             dropDown
+//                 .append("option")
+//                 .text(sample)
+//                 .property("value", sample);
+//         });
+
+//         var firstSample = sampleNames[0];
+//         buildCharts(firstSample);
+//         buildMetadata(firstSample);
+
+//     });
+// }
+
+// function optionChanged(newSample) {
+//     buildCharts(newSample);
+//     buildMetadata(newSample);
+// }
+
+// init();
+
+// // Display the sample Metadata
+// function buildMetadata(sample) {
+//     d3.json("samples.json").then((data) => {
+//         var sampleMeta = data.metadata;
+        
+//         var results = sampleMeta.filter(sampleObj => sampleObj.id == sample)[0];
+
+//         var panelData = d3.select("#sample-metadata");
+
+//         panelData.html("");
+
+//         Object.entries(results).forEach(([key, value]) => {
+//             panelData.append("h6").text(`${key} : ${value}`);
+
+//         });
+//     });
+// }
